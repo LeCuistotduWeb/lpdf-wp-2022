2fa8c44fa767226ee4ef49d3997d1a8c4381c061
Gaetan Boyron <gaetan.boyron@blissim.fr>
2022-05-04 20:13:49 +0200

    feat: menu mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,40 @@
+// Toggle menu mobile
+const menuMobileButton = document.getElementById('menuMobileButton')
+const menuContainer = document.getElementById('headerMenu')
+menuMobileButton.addEventListener('click', (e) => {
+  e.preventDefault()
+  menuContainer.classList.toggle('open')
+})
+
 // Dealy function
 function delay(callback, ms) {
-  var timer = 0;
+  var timer = 0
   return function() {
-    var context = this, args = arguments;
-    clearTimeout(timer);
+    var context = this, args = arguments
+    clearTimeout(timer)
     timer = setTimeout(function () {
-      callback.apply(context, args);
-    }, ms || 600);
-  };
+      callback.apply(context, args)
+    }, ms || 600)
+  }
 }
 
 // Search input
-const searchInput = document.getElementsByClassName('input-search');
-searchInput.addEventListener('click', delay(() => {
-  const inputValue = searchInput.val();
-  const searchResponse = document.getElementsByClassName('search-response');
-  console.log()
+const searchInput = document.getElementById('navbarSearch')
+searchInput.addEventListener('click', delay((e) => {
+  const inputValue = searchInput.val()
+  const searchResponse = document.getElementsByClassName('search-response')
   if(inputValue.length >= 3){
-    searchResponse.html('Recherche en cours...');
+    searchResponse.html('Recherche en cours...')
     fetch('?action=search',{
       method: 'POST',
       headers: new Headers(),
       mode: 'cors',
       body: {value: inputValue },
       cache: 'default' })
-      .then(function(response) {
-        return searchResponse.html(response);
-      }).catch(err => searchResponse.html(err));
+        .then(function(response) {
+          return searchResponse.html(response)
+        }).catch(err => searchResponse.html(err))
   } else {
     searchResponse.html('')
   }
-}));
+}))
