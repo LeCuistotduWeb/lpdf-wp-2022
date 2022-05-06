d2fad9171d262e355f776fc820808f25c1401e8c
Gaetan Boyron <gaetan.boyron@blissim.fr>
2022-05-06 09:59:42 +0200

    feat: archive page

---
+++ archive.php
@@ -0,0 +1,64 @@
+<?php get_header(); ?>
+
+<main role="main">
+    <header class="category-header" style="background-color: #FFDCD9">
+        <div class="container">
+            <h1 class="category-header__title">
+                <div class="category-header__overline">Catégories</div>
+                <div class="category-header__name"><?php single_cat_title() ?></div>
+            </h1>
+        </div>
+      <?php the_archive_description('<div class="category__description container">','</div>'); ?>
+    </header>
+
+    <div class="category-page__container">
+        <div class="container">
+          <div class="category-page__post-list">
+            <?php if(have_posts()):
+            while (have_posts()): the_post(); ?>
+
+                <article class="post-card post-card-large">
+                    <a class="post-card__link" href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>">
+                        <div class="post-card__container">
+                            <div class="post-card__thumbnail">
+                                <img width="200" src="<?php the_post_thumbnail_url() ?>" alt="<?php the_title(); ?>">
+                                <div class="post-card__info">
+                                    <div class="post-card__info-likes">
+                                      <?= getPostLikes(get_the_ID()) ?>
+                                    </div>
+                                    <div class="post-card__info-comments">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15.732 13.765">
+                                            <path id="Icon_awesome-comment" data-name="Icon awesome-comment" d="M7.866,2.25C3.521,2.25,0,5.111,0,8.641a5.672,5.672,0,0,0,1.751,4.016A7.817,7.817,0,0,1,.068,15.6a.244.244,0,0,0-.046.267.241.241,0,0,0,.224.147,7.027,7.027,0,0,0,4.32-1.579,9.371,9.371,0,0,0,3.3.6c4.345,0,7.866-2.861,7.866-6.391S12.21,2.25,7.866,2.25Z" transform="translate(0 -2.25)" fill="#232323"/>
+                                        </svg>
+                                        <span><?= get_comments_number() ?></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="post-card__content">
+                                <div class="post-card__categories"><?php the_category(', ') ?></div>
+                                <h3 class="post-card__title"><?php the_title(); ?></h3>
+                                <p class="post-card__excerpt"><?php the_excerpt(); ?></p>
+                                <a class="post-card__link" href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>">Lire l'article</a>
+                            </div>
+                        </div>
+                    </a>
+                </article>
+
+            <?php endwhile; ?>
+
+              <?php customPagination(); ?>
+
+          <?php else: ?>
+              <div>
+                  <p>Il n'y a aucun resultat</p>
+              </div>
+          <?php endif; ?>
+          </div>
+        </div>
+    </div>
+
+</main>
+
+<?php get_footer(); ?>
+
+
--- functions.php
+++ functions.php
@@ -46,8 +46,6 @@ function removeWcAssets()
 }
 add_action( 'get_header', 'removeWcAssets' );
 
-require_once('classes/custom-post-likes.class.php');
-
 /**
  * Register widget areas
  */
@@ -96,9 +94,49 @@ function widgetsInit()
 add_action('widgets_init', 'widgetsInit');
 
 /**
+ * Excerpt length limit
+ * @param $length
+ * @return int
+ */
+function custom_excerpt_length( $length ) {
+  return 20;
+}
+add_filter( 'excerpt_length', 'custom_excerpt_length', 10);
+
+/**
+ * Custom pagination
+ * @return void
+ */
+function customPagination()
+{
+  global $wp_query;
+  $big = 999999999;
+  echo '<div class="pagination">' . paginate_links([
+    'base' => str_replace($big, '%#%', get_pagenum_link($big)),
+    'format' => '?paged=%#%',
+    'current' => max(1, get_query_var('paged')),
+    'total' => $wp_query->max_num_pages,
+    'mid_size' => 1,
+    'prev_next' => true,
+    'prev_text' => '<svg xmlns="http://www.w3.org/2000/svg" width="16.216" height="26.26" viewBox="0 0 16.216 26.26">
+  <path id="Icon_material-keyboard-arrow-left" data-name="Icon material-keyboard-arrow-left" d="M28.216,31.425,18.193,21.38,28.216,11.336,25.13,8.25,12,21.38,25.13,34.51Z" transform="translate(-12 -8.25)" fill="#e0cec7"/>
+</svg>',
+    'next_text' => '<svg xmlns="http://www.w3.org/2000/svg" width="16.216" height="26.26" viewBox="0 0 16.216 26.26">
+  <path id="Icon_material-keyboard-arrow-left" data-name="Icon material-keyboard-arrow-left" d="M16.216,23.175,6.193,13.13,16.216,3.086,13.13,0,0,13.13,13.13,26.26Z" transform="translate(16.216 26.26) rotate(180)" fill="#e0cec7"/>
+</svg>'
+  ]) . '</div>';
+}
+add_action('init', 'customPagination');
+
+/**
  * Transient functions
  */
 require_once('classes/transient.class.php');
+
+/**
+ * Post-like Classe
+ */
+require_once('classes/custom-post-likes.class.php');
 
 /**
  * Shortcodes functions
@@ -109,3 +147,8 @@ require_once('functions/shortcodes.php');
  * Brand functions
  */
 require_once('functions/brand.php');
+
+/**
+ * Search functions
+ */
+require_once('functions/search.php');
